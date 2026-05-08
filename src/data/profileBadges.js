e5410b23e55129fe957795c5f9d0ab904350e6ba
claudya <claudyacr21@gmail.com>
2026-05-08 19:10:35 +0700

    update

--- src/data/profileBadges.js
+++ src/data/profileBadges.js
@@ -1,6 +1,6 @@
 const PROFILE_BADGES = {
   motherFlame: process.env.MOTHER_FLAME_BADGE_EMOJI || "<:motherflame:1502256240267497522>",
-  serverBooster: process.env.SERVER_BOOSTER_BADGE_EMOJI || "<:serverbooster:1502280321457197228>",
+  serverBooster: process.env.SERVER_BOOSTER_BADGE_EMOJI || "<:serverbooster:1502281385602449520>",
 };
 
 const RAID_PRESTIGE_BADGE_EMOJIS = {
