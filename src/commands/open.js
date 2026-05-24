1c342178352803f15dd7db4be97bcd545f10a5fb
claudya <claudyacr21@gmail.com>
2026-05-24 16:14:07 +0700

    update

--- src/commands/open.js
+++ src/commands/open.js
@@ -311,7 +311,7 @@ function grantBoxRewards(box, amount, state, rewardMap) {
       );
     }
 
-    if (Math.random() < 0.20) {
+    if (Math.random() < 0.05) {
       addReward(ITEMS.pullResetTicket, 2);
     }
   } else if (box.code === "mother_flame_treasure_box") {
