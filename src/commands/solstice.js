ea20d18bd51f9516b0d06069d6fa487d97a03977
claudya <claudyacr21@gmail.com>
2026-08-01 23:10:43 +0700

    Display equipped custom skins in Solstice battles

--- src/commands/solstice.js
+++ src/commands/solstice.js
@@ -26,10 +26,6 @@ const {
   buildMergedCard,
 } = require("../utils/mergeCards");
 
-const {
-  applyCustomSkinToCard,
-} = require("../utils/customSkins");
-
 const EVENT_ID = "midsummer_2026";
 const GLOBAL_STORE_ID = "__midsummer_2026_global";
 
@@ -608,14 +604,8 @@ function getBattleTeam(player) {
             )
           : hydrateCard(ownedCard);
 
-      const displayCard =
-        applyCustomSkinToCard(
-          player,
-          syncedCard
-        );
-
       return applyTeamBoosts(
-        displayCard,
+        syncedCard,
         boosts
       );
     })
